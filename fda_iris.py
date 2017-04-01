2a4bf7f9be797fb054069e12b2c5e376d25bbaab
D-e-e-m-o <wsd009811@gmail.com>
2017-04-01 16:38:21 +0800

    getS() bug fixed

--- fda_iris.py
+++ fda_iris.py
@@ -17,11 +17,12 @@ def getData(dataFile):
 					classes[tmp] = [dataTmp]
 				else:
 					classes[tmp].append(dataTmp)
-		# data = np.asarray(data, dtype='float')
+				# data = np.asarray(data, dtype='float')
 	finally:
 		file.close()
 	nl = len(data)
 	return data, classes, nl
+
 
 def getW(data, classes, nl):
 	# 求权值矩阵Wb和Ww
@@ -37,29 +38,33 @@ def getW(data, classes, nl):
 			tmpWw.append([])
 			for j in range(nl):
 				if (data[i] in classes[cl]) and (data[j] in classes[cl]):
-					tmpWb[-1].append(1/nl-1/nk)
-					tmpWw[-1].append(1/nk)
+					tmpWb[-1].append(1 / nl - 1 / nk)
+					tmpWw[-1].append(1 / nk)
 				else:
-					tmpWb[-1].append(1/nl)
+					tmpWb[-1].append(1 / nl)
 					tmpWw[-1].append(0)
 		Wb[cl] = np.asarray(tmpWb)
 		Ww[cl] = np.asarray(tmpWw)
 	return Wb, Ww
 
+
 def getS(data, Wb, Ww, nl):
 	Sb = {}
 	Sw = {}
 	x = np.asarray(data)
+	length = x[0].shape[0]
 	for category in Wb.keys():
-		Sb[category] = np.asarray([])
-		Sw[category] = np.asarray([])
+		Sb[category] = np.zeros([4, 4], dtype='float')
+		Sw[category] = np.zeros([4, 4], dtype='float')
 		for i in range(nl):
 			for j in range(nl):
-				Sb[category] += Wb[category][i][j] * np.dot(x[i]-x[j], (x[i]-x[j]).T)
-				Sw[category] += Ww[category][i][j] * np.dot(x[i]-x[j], (x[i]-x[j]).T)
+				tmp = (x[i] - x[j]).reshape(length, 1)
+				Sb[category] += Wb[category][i][j] * tmp * tmp.T
+				Sw[category] += Ww[category][i][j] * np.dot(tmp, tmp.T)
 		Sb[category] *= 0.5
 		Sw[category] *= 0.5
 	return Sb, Sw
+
 
 if __name__ == '__main__':
 	dataFile = 'iris.data'
