b507c51d638929cd6b7b072ea051ea72c86306fc
D-e-e-m-o <wsd009811@gmail.com>
2017-04-24 17:13:52 +0800

    字符串映射成数字

--- fda_kddcup.py
+++ fda_kddcup.py
@@ -1,25 +1,34 @@
 #!/usr/bin/env python3
-# 6 7 8 13 14 16-20 41
-# tcp等等要编号映射
+"""
+6 7 8 13 14 16-20 41
+tcp等等要编号映射
+第一列--tcp:1,udp:2,icmp:3
+第二列--['http', 'smtp', 'finger', 'domain_u', 'auth', 'telnet', 'ftp', 'eco_i', 'ntp_u', 'ecr_i', 'other', 'private', 'pop_3', 'ftp_data', 'rje', 'time', 'mtp', 'link', 'remote_job', 'gopher', 'ssh', 'name', 'whois', 'domain', 'login', 'imap4', 'daytime', 'ctf', 'nntp', 'shell', 'IRC', 'nnsp', 'http_443', 'exec', 'printer', 'efs', 'courier', 'uucp', 'klogin', 'kshell', 'echo', 'discard', 'systat', 'supdup', 'iso_tsap', 'hostnames', 'csnet_ns', 'pop_2', 'sunrpc', 'uucp_path', 'netbios_ns', 'netbios_ssn', 'netbios_dgm', 'sql_net', 'vmnet', 'bgp', 'Z39_50', 'ldap', 'netstat', 'urh_i', 'X11', 'urp_i', 'pm_dump', 'tftp_u', 'tim_i', 'red_i']
+第三列--['SF', 'S1', 'REJ', 'S2', 'S0', 'S3', 'RSTO', 'RSTR', 'RSTOS0', 'OTH', 'SH']
+"""
 import fda_iris as fda
 import numpy as np
 
 
 def getData(dataFile):
 	file = open(dataFile, 'r')
+	line1 = ['tcp', 'udp', 'icmp']
+	line2 = ['http', 'smtp', 'finger', 'domain_u', 'auth', 'telnet', 'ftp', 'eco_i', 'ntp_u', 'ecr_i', 'other', 'private', 'pop_3', 'ftp_data', 'rje', 'time', 'mtp', 'link', 'remote_job', 'gopher', 'ssh', 'name', 'whois', 'domain', 'login', 'imap4', 'daytime', 'ctf', 'nntp', 'shell', 'IRC', 'nnsp', 'http_443', 'exec', 'printer', 'efs', 'courier', 'uucp', 'klogin', 'kshell', 'echo', 'discard', 'systat', 'supdup', 'iso_tsap', 'hostnames', 'csnet_ns', 'pop_2', 'sunrpc', 'uucp_path', 'netbios_ns', 'netbios_ssn', 'netbios_dgm', 'sql_net', 'vmnet', 'bgp', 'Z39_50', 'ldap', 'netstat', 'urh_i', 'X11', 'urp_i', 'pm_dump', 'tftp_u', 'tim_i', 'red_i']
+	line3 = ['SF', 'S1', 'REJ', 'S2', 'S0', 'S3', 'RSTO', 'RSTR', 'RSTOS0', 'OTH', 'SH']
 	try:
 		data = []
-		classes = {}
-		for i in range(10000):
+		classes = {'normal': [], 'bad': []}
+		for i in range(300):
 			line = file.readline()
 			if line != '\n':
-				dataTmp = [float(i) for i in line.split(',')[4:6]]
+				tmp = line.split(',')
+				dataTmp = [line1.index(tmp[1])+1, line2.index(tmp[2])+1, line3.index(tmp[3])+1,\
+							float(tmp[4]), float(tmp[5])]
 				data.append(dataTmp)
-				tmp = line.split(',')[-1][:-2]
-				if not tmp in classes:
-					classes[tmp] = [dataTmp]
+				if tmp[-1][:-2] == 'normal':
+					classes['normal'].append(dataTmp)
 				else:
-					classes[tmp].append(dataTmp)
+					classes['bad'].append(dataTmp)
 				# data = np.asarray(data, dtype='float')
 	finally:
 		file.close()
